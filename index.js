42a219353aae913ec4a4c26b32cdd3fb159aad9d
JasonNaggles <JasonNaggles.verizon.net>
2023-08-26 20:51:23 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+class Friend {
+    constructor(firstName, lastName, gender, id) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.id = id;
+    }
+}
